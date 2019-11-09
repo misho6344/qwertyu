3135358116df0f248686334bc2bd185e13d3e1d3
misho6344 <56062750+misho6344@users.noreply.github.com>
2019-11-09 11:49:56 +0400

    Add files via upload

---
+++ project 1.kt
@@ -0,0 +1,68 @@
+
+
+//inheritance
+
+
+
+//import java.io.CharArrayReader
+//
+//
+//
+//open class human {
+//    open fun think(){
+//        println("შეუძლია ფიქრი")
+//    }
+//}
+//open class Alien : human(){
+//    override fun think(){
+//        println("შეუძლია განსხვავებულად ფიქრი")
+//    }
+//}
+//open class computer : Alien(){
+//    fun virtual(){
+//        println("ვირტუალური ფიქრი")
+//    }
+//}
+//class robot:computer(){
+//    fun robotulad(){
+//        println("რობოტივით სიარული")
+//    }
+//
+//
+//}
+//
+//fun main(args: Array<String>)
+//{
+//    var gela = Alien()
+//    gela.think()
+//    var MRX = computer()
+//    MRX.virtual()
+//    var Robokopi = robot()
+//    Robokopi.robotulad()
+//
+//
+//}
+
+
+
+//dataclass
+//
+//
+//
+//
+//data public class Person(val age:Int, val firstName:String, var lastname:String)
+//fun main(args: Array<String>) {
+//    val Person1 = Person(18,"misho", "abuladze")
+//    val person2 = Person(age= 21,firstName = "saxeli",lastname = "gvari")
+//    val person3 = Person1
+//
+//    println(Person1)
+//    println(Person1.hashCode())
+//    println(Person1.equals(person2))
+//    println(Person1.equals(person3))
+//}
+//
+//
+
+
+
